228c9e908dfa98052ff597b0235e3add33c99979
LisaW <LisaW@ya.ru>
2022-05-01 10:52:31 +0300

    feat: data structures implementation

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,46 +1,148 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
-/**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
 class BinarySearchTree {
-
+  constructor() {
+    this.treeRoot = null;
+  }
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+    return this.treeRoot;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+
+    let newNode = new Node(data);
+    if (this.root() == null){
+      this.treeRoot = newNode;
+    }
+    else{
+      let node = this.root();
+      while (node!==null){
+        if (node.data > data){
+          if (node.left == null) { 
+            node.left = newNode; 
+            return newNode;
+          };
+          node = node.left;
+        } else {
+          if (node.right == null) {
+            node.right = newNode;
+            return newNode;
+          };
+          node = node.right
+        };
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    let node = this.root();
+     while (node!==null){
+       if (node.data == data) return true;
+       if(node.data > data){
+         node = node.left; 
+       } else node = node.right;
+     }
+     return false;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+
+     let node = this.root();
+     while (node !== null) {
+       if (node.data == data) return node;
+       if (node.data > data) {
+         node = node.left;
+       } else node = node.right;
+     }
+     return null;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+
+    // if tree is empty
+    if (this.root() === null) return null;
+    // init values to search from root;
+    let currentNode = this.root();
+    let parentNode = this.root();
+    let direction;
+    //find null (element doesn't exit) or parent and Node with data;
+    while ( currentNode.data !== null && currentNode.data !== data){
+      parentNode = currentNode;
+      if( currentNode.data > data ){
+        currentNode = currentNode.left;
+        direction = "left";
+      }
+      else{
+        currentNode = currentNode.right;
+        direction="right";
+      }
+    };
+    // exit when data didn't match tree
+    if (currentNode === null) return null;
+    //replace node by branch if it doesn't have right or left child
+    if( currentNode.right === null) {
+      if (parentNode===currentNode) {
+        this.treeRoot = currentNode.left;
+        }
+      else{
+        parentNode[direction] = currentNode.left;
+      }
+    }
+    else if (currentNode.left === null) 
+    {
+      if (parentNode === currentNode) {
+        this.treeRoot = currentNode.right;
+      } else {
+        parentNode[direction] = currentNode.right;
+      }
+    }
+    // find leftest value in right subtree to replace deleting value;
+    else{
+      let forReplaceNode = currentNode.right;
+      let forReplaceNodeParent = currentNode.right;
+      while (forReplaceNode.left!==null){
+        if (forReplaceNode.left.left!==null) forReplaceNodeParent = forReplaceNodeParent.left;
+        forReplaceNode = forReplaceNode.left;
+
+      };
+      forReplaceNodeParent.left = forReplaceNode.right;
+      forReplaceNode.left = currentNode.left;
+      forReplaceNode.right = currentNode.right;
+      if (parentNode.left === currentNode ){
+        parentNode.left = forReplaceNode;
+      }
+      else{
+        parentNode.right = forReplaceNode;
+      }
+      if (this.root() === currentNode) this.treeRoot = forReplaceNode;
+
+
+    }
+
+
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+    let node = this.root();
+    if ( node === null ) return null;
+    while (node.left !== null) {
+      node = node.left;
+    }
+    return node.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+    let node = this.root();
+    if (node === null) return null;
+    while (node.right!==null){
+      node = node.right;
+    }
+    return node.data;
   }
 }
 
--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,9 +22,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList( l, k ) {
+  // console.debug("l ", l);
+
+  if( l === null ) return false;
+  let head = l;
+  let curr = l;
+  let next = l.next;
+
+  while (next!==null) {
+    if (head.value === k ){
+      head = head.next;
+      curr = head;
+      next = curr.next;
+    }
+    if (next.value === k){
+      curr.next = next.next;
+      next = curr.next;
+    } else{
+      curr = curr.next;
+      next = curr.next;
+    }
+  }
+    return head;
 }
 
 module.exports = {
